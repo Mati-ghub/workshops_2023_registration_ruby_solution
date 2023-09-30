e0d463f7c075b6ed561a12ad735c9e8028627c6d
Mateusz Piłat <matson@MacBook-Pro-Mateusz.local>
2023-09-30 22:10:57 +0200

    Add solution

--- count_elements.rb
+++ count_elements.rb
@@ -1,3 +1,17 @@
 class CountElements
+  def initialize(results)
+    @results = results
+  end
 
+  def perform
+    hash = {}
+
+    results.each do |element|
+      hash[element] = results.count(element)
+    end
+
+    hash
+  end
+
+  attr_accessor :results
 end
--- validate_password.rb
+++ validate_password.rb
@@ -1,3 +1,28 @@
-class ValidatePassword
+require 'pry'
 
+class ValidatePassword
+  def initialize(password)
+    @password = password
+  end
+
+  def perform
+    return false if password.length > 26 || password.length < 6
+    return false if (password =~ /[A-Z]/).nil?
+    return false if (password =~ /[a-z]/).nil?
+    return false if (password =~ /\d/).nil?
+    return false if special_character_present?
+    return false if password.scan(/(.)\1{2,}/).any?
+
+    true
+  end
+
+  attr_accessor :password
+
+  private
+
+  def special_character_present?
+    char_array = %w(! @ # $ % & * + = : ; ? < >)
+
+    return password.split.map { |letter| char_array.include?(letter) }.any?(true)
+  end
 end
